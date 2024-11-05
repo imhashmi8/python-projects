e88b96b47de97bb194156d37105b2aa182c83875
Qamar.Hashmi <qamar.hashmi@outlook.com>
2024-11-05 16:47:11 +0530

    added Band-name-Generator

---
+++ Band-Name-Generator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator.")
+city = input("What's the city name you grew up in ?\n")
+pet = input("What is your pet name ?\n")
+print("Your Band name could be " + city + " " + pet)
