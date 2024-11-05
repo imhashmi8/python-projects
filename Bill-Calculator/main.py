43f3c1a42302ccb71510a72ee213fd748950c94e
Qamar.Hashmi <qamar.hashmi@outlook.com>
2024-11-06 00:45:47 +0530

    Added Bill calculator

--- Bill-Calculator/main.py
+++ Bill-Calculator/main.py
@@ -3,8 +3,11 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 tip_as_percent = tip / 100
-total_bill = bill * tip_as_percent + bill
-total_bill_per_person = round(total_bill / people)
-print(f"Each person needs to pay :  +${total_bill_per_person}")
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+print(f"Each person should pay: ${final_amount}")
+
 
 
